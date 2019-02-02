d3fbe37238fae5f4a755a43d658f34c70f96706f
Hatchin <sshen7@usfca.edu>
2019-02-01 17:03:45 -0800

    tweak

--- app.py
+++ app.py
@@ -2,20 +2,20 @@ import os
 import requests
 from flask import Flask, render_template, request
 from main import *
-#from flask.ext.sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
-#app.config.from_object(os.environ['APP_SETTINGS'])
-#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-#db = SQLAlchemy(app)
-
-#from models import Result
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     errors = []
     results = {}
+    data = ''
+    t_title = ''
+    signif = ''
+
+
     if request.method == "POST":
         # get url that the user has entered
         try:
